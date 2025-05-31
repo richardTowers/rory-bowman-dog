a628ce7adb1478b1ce6f21fd5c90730527628845
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-05-31 19:10:35 +0100

    Set viewport

--- template.tsx
+++ template.tsx
@@ -40,6 +40,7 @@ export function GalleryPage({ images }: { images: ImageMeta[] }) {
         <meta charSet="UTF-8" />
         <title>Photo Gallery</title>
         <link rel="stylesheet" href="style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <div className="row">
